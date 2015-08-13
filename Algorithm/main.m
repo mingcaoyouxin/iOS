ee6dcd5065d2e003aeeb9273773a66fc6c220ee1
mingcaoyouxin <mingcaoyouxin@gmail.com>
2015-08-13 22:06:38 +0800

    Initial Commit

---
+++ Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by 浩 董 on 15/8/13.
+//  Copyright (c) 2015年 com.tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
